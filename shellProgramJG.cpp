5d7828f579207628e3b1863ab653ff913f401453
Josh Gilstorf <gilstorfj2@mailbox.winthrop.edu>
2025-02-05 13:50:04 -0500

    Part 1

--- shellProgramJG.cpp
+++ shellProgramJG.cpp
@@ -4,39 +4,53 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sstream>
 
 using namespace std;
 
 int main() {
     string input;
+    fstream history;
+    history.open("historyFile.txt", ios::in);
     cout << "InsertFunnyPromptHere:\\$";
     //Get the line, if = myprocess, call getpid, else, system(fjnifs)
     getline(cin, input);
+    history << input << endl;
     if (input == "myprocess") {
         system("ps -u");
     }
     else if (input == "allprocesses") {
         system("ps -a");
     }
-    else if (input == "chgd") {
-        system("chgd");
+    else if (input.substr(0, 4) == "chgd") { //Take up to first space -> command, param
+        string command;
+        string params;
+        stringstream inputstream(input);
+        inputstream >> command >> params;
+        system("chdir(params.c_str())");
     }
     else if (input == "clr") {
-        system("clr");
+        system("clear");
     }
     else if (input == "dir") {
-        system("dir");
+        system("ls");
     }
     else if (input == "environ") {
-        system("environ");
+        system("env");
     }
     else if (input == "quit") {
+        history.close();
+        history.open("historyFile.txt", ios::out);
+        string content;
+        while(getline(history, content)) {
+            cout << content;
+        }
         system("quit");
     }
     else if (input == "help") {
-        system("help");
+        system("help"); //cout functions for YOUR shell 
     }
     else {
-        system(input.c_str());
+        system("input.c_str()");
     }
 }
